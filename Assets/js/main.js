8bec91b638c97f27eb6adfa755b2dcc095745d5d
Fabio-Colombo <thesmallworld95@gmail.com>
2021-04-06 01:23:51 +0200

    Completata Milestone 3

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,6 +8,7 @@ var app = new Vue (
 				"./Assets/img/RubeusHagrid.jpg",
 				"./Assets/img/HermioneGranger.jpg",
 			],
+      inputTxt: "",
       index: 0,
       contacts: contacts
     },
@@ -17,6 +18,39 @@ var app = new Vue (
         let newSchedule = schedule[1].split(":")
         return `${newSchedule[0]}:${newSchedule[1]}`;
       },
+      getLastAccess: function (index) {
+        let messages = this.contacts[index].messages;
+        let lastIndex = messages.length - 1;
+        return messages[lastIndex].date.split(" ")[1]
+      },
+      getLastMessage: function (index) {
+        let messages = this.contacts[index].messages;
+        let lastIndex = messages.length - 1;
+        return messages[lastIndex].text;
+      },
+      insertTxt: function () {
+        if (this.inputTxt != '') {
+          let indexProfile = this.index;
+          let today = dayjs().format('DD/MM/YY HH:mm:ss');
+          let sentMessage = {
+            date: today,
+            text: this.inputTxt,
+            status: 'sent'
+          }
+          this.contacts[indexProfile].messages.push(sentMessage)
+          this.inputTxt = "";
+          setTimeout(() => {
+            let today = dayjs().format('DD/MM/YY HH:mm:ss');
+            let receivedMessage = {
+              date: today,
+              text: 'ok',
+              status: 'received'
+            }
+            this.contacts[indexProfile].messages.push(receivedMessage)
+          }, 1000)
+        }
+
+      }
     }
   }
 );
